ba0b318dc0cf21b8007d1613163d75b0390125be
Fabio Schapowal <fabiosw@me.com>
2023-07-24 07:46:24 +0100

    packages2

---
+++ 10-Package2/main.py
@@ -0,0 +1,15 @@
+from sys import path
+path.append('/Users/fabioschapowal/Desktop/Python/Python_adv')
+
+print(path)
+
+import extra.iota
+print(extra.iota.funI())
+
+import extra.good.best.sigma as sig
+import extra.good.alpha as alp
+
+print(sig.FunS())
+print(alp.FunA())
+
+
