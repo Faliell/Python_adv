22cb95b1548142fedfc32d238a48e371b3a13099
Fabio Schapowal <fabiosw@me.com>
2023-07-28 06:51:08 +0100

    StringMethods

--- 13-StringMethods/main.py
+++ 13-StringMethods/main.py
@@ -131,6 +131,67 @@ print("This is it!".replace("is", "are", 1))
 print("This is it!".replace("is", "are", 2))
 
 
+### rfind()
+#começa pela direita
+print("tau tau tau".rfind("ta"))
+print("tau tau tau".rfind("ta", 9))
+print("tau tau tau".rfind("ta", 3, 9))
 
 
+### rstrip()
+print("[" + " upsilon ".rstrip() + "]")
+print("python.com".rstrip(".com"))
+
+
+### split()
+print("phi       chi\npsi".split())
+
+
+### startswith()
+print("omega".startswith("meg"))
+print("omega".startswith("om"))
+
+
+### strip()
+print("[" + "   aleph   ".strip() + "]")
+
+
+### swapcase()
+print("I know that I know nothing.".swapcase())
+
+
+### title()
+print("I know that I know nothing. Part 1.".title())
+
+
+### upper()
+print("I know that I know nothing. Part 2.".upper())
+
+
+"""Some of the methods offered by strings are:
+
+capitalize() – changes all string letters to capitals;
+center() – centers the string inside the field of a known length;
+count() – counts the occurrences of a given character;
+join() – joins all items of a tuple/list into one string;
+lower() – converts all the string's letters into lower-case letters;
+lstrip() – removes the white characters from the beginning of the string;
+replace() – replaces a given substring with another;
+rfind() – finds a substring starting from the end of the string;
+rstrip() – removes the trailing white spaces from the end of the string;
+split() – splits the string into a substring using a given delimiter;
+strip() – removes the leading and trailing white spaces;
+swapcase() – swaps the letters' cases (lower to upper and vice versa)
+title() – makes the first letter in each word upper-case;
+upper() – converts all the string's letter into upper-case letters.
+
+2.(all of them return Boolean values):
+
+endswith() – does the string end with a given substring?
+isalnum() – does the string consist only of letters and digits?
+isalpha() – does the string consist only of letters?
+islower() – does the string consists only of lower-case letters?
+isspace() – does the string consists only of white spaces?
+isupper() – does the string consists only of upper-case letters?
+startswith() – does the string begin with a given substring?"""
 
