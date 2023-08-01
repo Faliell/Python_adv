0a8526ec33fb05b5f44b4a5f3599ecbd02ebee98
Fabio Schapowal <fabiosw@me.com>
2023-08-01 07:34:32 +0100

    Simple Programs

---
+++ 14-StringsInActions/main.py
@@ -0,0 +1,64 @@
+print('alpha' == 'alpha')
+print('alpha' != 'Alpha')
+
+
+# idêntica ao início da mais longa, a string mais longa é considerada maior.
+print('alpha' < 'alphabet')
+
+# letras maiúsculas são consideradas menores que as minúsculas
+print('beta' > 'Beta')
+
+# se uma string contém apenas dígitos, ainda não é um número.
+print('10' == '010')
+print('10' > '010')
+print('10' > '8')
+print('20' < '8')
+print('20' < '80')
+
+# Comparar strings com números geralmente é uma má ideia.
+# únicas comparações  == e !=
+print('10' == 10)
+print('10' != 10)
+print('10' == 1)
+print('10' != 1)
+#print('10' > 10)
+
+
+# Sorted() - retorna nova lista, não altera original
+
+first_greek = ['omega', 'alpha', 'pi', 'gamma']
+first_greek_2 = sorted(first_greek)
+
+print(first_greek)
+print(first_greek_2)
+
+print()
+
+# sort() - altera original
+second_greek = ['omega', 'alpha', 'pi', 'gamma']
+print(second_greek)
+
+second_greek.sort()
+print(second_greek)
+
+
+#conversão
+
+itg = 13
+flt = 1.3
+si = str(itg)
+sf = str(flt)
+
+print(si + ' ' + sf)
+
+# reverso:
+si = '13'
+sf = '1.3'
+itg = int(si)
+flt = float(sf)
+#errado = int('1.3')
+print(itg + flt)
+
+
+
+
---
+++ 15-Simple programs/main.py
@@ -0,0 +1,114 @@
+# Caesar cipher.
+text = input("Enter your message: ")
+cipher = ''
+for char in text:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) + 1
+    if code > ord('Z'):
+        code = ord('A')
+    cipher += chr(code)
+
+print(cipher)
+
+
+# Caesar cipher - decrypting a message.
+cipher = input('Enter your cryptogram: ')
+text = ''
+for char in cipher:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) - 1
+    if code < ord('A'):
+        code = ord('Z')
+    text += chr(code)
+
+print(text)
+
+
+
+# Numbers Processor.
+line = input("Enter a line of numbers - separate them with spaces: ")
+strings = line.split()
+total = 0
+try:
+    for substr in strings:
+        total += float(substr)
+    print("The total is:", total)
+except:
+    print(substr, "is not a number.") #corrigir erro
+
+
+
+# IBAN Validator.
+
+"""Remover todos os espaços em branco e caracteres não numéricos do IBAN.
+Mover os quatro caracteres iniciais do IBAN para o final.
+Substituir as letras no IBAN por números, de acordo com a tabela de
+conversão de letras (A=10, B=11, ..., Z=35).
+Verificar se o IBAN resultante é divisível por 97."""
+
+iban = input("Enter IBAN, please: ")
+iban = iban.replace(' ','')
+
+if not iban.isalnum():
+    print("You have entered invalid characters.")
+elif len(iban) < 15:
+    print("IBAN entered is too short.")
+elif len(iban) > 31:
+    print("IBAN entered is too long.")
+else:
+    iban = (iban[4:] + iban[0:4]).upper()
+    iban2 = ''
+    for ch in iban:
+        if ch.isdigit():
+            iban2 += ch
+        else:
+            iban2 += str(10 + ord(ch) - ord('A'))
+    iban = int(iban2)
+    if iban % 97 == 1:
+        print("IBAN entered is valid.")
+    else:
+        print("IBAN entered is invalid.")
+
+
+
+#Palindromes:
+"""Your task is to write a program which:
+
+asks the user for some text;
+checks whether the entered text is a palindrome, and prints the result."""
+
+text = input("Enter text: ")
+
+# Remove all spaces...
+text = text.replace(' ','')
+
+# ... and check if the word is equal to reversed itself
+if len(text) > 1 and text.upper() == text[::-1].upper():
+	print("It's a palindrome")
+else:
+	print("It's not a palindrome")
+
+
+
+#Anagrams:
+
+"""Exemplo 1:
+
+Anagrama: "amor" e "roma"
+Exemplo 2:
+
+Anagrama: "listen" e "silent"""
+
+str_1 = input("Enter the first string: ")
+str_2 = input("Enter the second string: ")
+
+strx_1 = ''.join(sorted(list(str_1.upper().replace(' ',''))))
+strx_2 = ''.join(sorted(list(str_2.upper().replace(' ',''))))
+if len(strx_1) > 0 and strx_1 == strx_2:
+	print("Anagrams")
+else:
+	print("Not anagrams")
