4574134dbbcd9c589c614491c0f688ef71a6f9db
Fabio Schapowal <fabiosw@me.com>
2023-08-29 21:42:39 +0100

    calendar

--- 30-Calendar/main2.py
+++ 30-Calendar/main2.py
@@ -15,3 +15,84 @@ calendar.LocalHTMLCalendar – é uma subclasse da classe calendar.HTMLCalendar.
 O construtor desta classe usa o parâmetro locale, que é usado para 
 os nomes apropriados dos meses e dias da semana.
 """
+
+
+""" Criando um objeto Calendário
+ 
+Recebe um parâmetro opcional, firstweekday, por padrão igual a 0 (segunda-feira). 
+ """
+
+import calendar
+
+c = calendar.Calendar(calendar.SUNDAY)
+
+for weekday in c.iterweekdays():
+    print(weekday, end=" ")
+
+""" iterweekdays, que retorna um iterador para números de dias da semana. 
+"""
+
+
+""" itermonthdates()
+ 
+Como resultado, todos os dias do mês e ano especificados são retornados,
+bem como todos os dias anteriores ao início ou ao final do mês que são
+necessários para obter uma semana completa.
+Cada dia é representado por um objeto datetime.date
+"""
+
+import calendar
+
+c = calendar.Calendar()
+
+for date in c.itermonthdates(2019, 11):
+    print(date, end=" ")
+
+
+""" itermonthdates() """
+
+import calendar
+
+c = calendar.Calendar()
+
+for iter in c.itermonthdays(2019, 11):
+    print(iter, end=" ")
+
+
+""" 
+itermonthdates2 – retorna dias na forma de tuplas consistindo de um dia
+do número do mês e um número do dia da semana;
+itermonthdates3 – retorna dias na forma de tuplas que consistem em números de ano,
+mês e dia do mês. 
+itermonthdates4 – retorna dias na forma de tuplas que consistem em números de ano,
+mês, dia do mês e dia da semana. 
+"""
+
+print()
+""" monthdays2calendar()
+ 
+retorna uma lista de listas, onde cada sublista representa uma semana no mês especificado.
+Cada elemento da sublista é uma tupla contendo um número que representa o dia do mês e um
+código que indica o dia da semana. 
+"""
+
+import calendar
+
+c = calendar.Calendar()
+
+for data in c.monthdays2calendar(2023, 8):
+    print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
