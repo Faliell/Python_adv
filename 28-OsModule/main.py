75ace9807c44549abe590164e15fa4e4e521600c
Fabio Schapowal <fabiosw@me.com>
2023-10-03 06:48:59 +0100

    OS

--- 28-OsModule/main.py
+++ 28-OsModule/main.py
@@ -1,22 +1,32 @@
 """ Obtendo informações sobre o sistema operacional """
 
 
-""" a função uname retorna um objeto contendo informações sobre o sistema operacional """
+"""
+uname
+A função retorna um objeto contendo informações sobre o sistema operacional
+
+A função uname só funciona em alguns sistemas Unix. Se você usa Windows,
+pode usar a função uname no módulo platform , que retorna um resultado semelhante. 
+"""
 
 
 import platform
 import os
-# print(os.uname()) # em unix
+#print(os.uname()) # em unix
 
 print(platform.uname()) #windows
 
 
 """ 
+O módulo "os" permite distinguir rapidamente o sistema operacional usando o
+atributo "name"
+
 os.name
     posix — you'll get this name if you use Unix;
     nt — you'll get this name if you use Windows;
     java — you'll get this name if your code is written in Jython."""
 print(os.name)
+
 
 
 """ criar diretorios"""
@@ -48,16 +58,23 @@ Se nenhum argumento for passado para ele, o diretório de trabalho atual será u
 """
 
 
-""" Recursive directory creation """
+""" 
+Criação de diretório recursivo 
+
+A função makedirs permite a criação recursiva de diretórios, o que significa
+que todos os diretórios no caminho serão criados. 
+"""
 
 
 #os.makedirs("my_first_directory/my_second_directory")
 #os.chdir("my_first_directory")
 print(os.listdir())
 
+""" Para mover entre diretórios, você pode usar uma função chamada chdir ,
+que altera o diretório de trabalho atual para o caminho especificado."""
+
 
 """Where am I now? getcwd"""
-
 
 #os.makedirs("my_first_directory/my_second_directory")
 os.chdir("my_first_directory")
@@ -66,7 +83,10 @@ os.chdir("my_second_directory")
 print(os.getcwd())
 
 
-""" Deleting directories in Python rmdir removedirs"""
+""" Excluindo diretórios em Python rmdir removedirs 
+
+Se um dos diretórios não existir ou não estiver vazio, uma exceção será gerada.
+"""
 
 
 # os.mkdir("my_first_directory")
@@ -88,7 +108,7 @@ print(returned_value)
 
 
 os.system("ls /Users/fabioschapowal/Desktop/Python/Python_adv/28-OsModule/my_first_directory")
-
+os.system("ls")
 
 """
 os.system("ls /caminho/para/seu/diretorio")  # No Linux/macOS
