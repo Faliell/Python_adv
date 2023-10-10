6b5e1ce61d5430e2d3ce686de3682daa28681dce
Fabio Schapowal <fabiosw@me.com>
2023-10-10 06:30:51 +0100

    Strings

---
+++ Resume/05-packagePygames.py
@@ -0,0 +1,63 @@
+"""
+https: // pypi.org /
+
+pip significa “pip instala pacotes”
+"""
+
+"""
+pip3 --version or pip --version
+pip help
+
+* pip help operation:
+pip help install
+pip list
+
+pip show pip
+
+quais pacotes são necessários para utilizar o pacote com sucesso ( Requires:)
+quais pacotes precisam que o pacote seja utilizado com sucesso ( Required-by:)
+
+- depreciado:
+pip search pip
+
++ --user instala somente para o usuário local, excluindo vai para todo sistema
+pip install --user pygame
+pip show pygame
+pip list
+
+*  -U significa update. 
+pip install -U package_name
+
+* versão exata
+pip install package_name==package_version
+pip install pygame==1.9.2
+
+pip uninstall package_name
+pip uninstall pygame
+
+"""
+
+""" 
+dependência é um fenômeno que aparece toda vez que você usa um software
+que depende de outro software . 
+
+O pip pode fazer tudo isso.
+"""
+
+import pygame
+
+run = True
+width = 400
+height = 100
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont(None, 48)
+text = font.render("Welcome to pygame", True, (255, 255, 255))
+screen.blit(text, ((width - text.get_width()) // 2, (height - text.get_height()) // 2))
+pygame.display.flip()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT \
+                or event.type == pygame.MOUSEBUTTONUP \
+                or event.type == pygame.KEYUP:
+            run = False
---
+++ Resume/06-Strings.py
@@ -0,0 +1,132 @@
+"""
+Os computadores armazenam caracteres como números. Há mais de uma maneira
+possível de codificar caracteres, mas apenas algumas delas ganharam popularidade
+mundial e são comumente usadas em TI: são elas ASCII (usado principalmente para
+codificar o alfabeto latino e alguns de seus derivados) e UNICODE
+(capaz de codificar virtualmente todos os alfabetos sendo usados por humanos).
+
+2. Um número correspondente a um caractere específico é chamado codepoint .
+
+3. UNICODE utiliza diferentes formas de codificação quando se trata de armazenar
+os caracteres em arquivos ou na memória do computador: duas delas são UCS-4 e UTF-8
+(esta última é a mais comum porque desperdiça menos espaço de memória).
+
+BOM (Byte Order Mark) é uma combinação especial de bits que anuncia
+a codificação usada pelo conteúdo de um arquivo (por exemplo, UCS-4 ou UTF-8).
+"""
+
+""" 
+Strings do Python são sequências imutáveis.
+
+Exemplos de len:
+"""
+word = 'by'
+print(len(word))
+
+empty = ''
+print(len(empty))
+
+i_am = 'I\'m'
+print(len(i_am))
+
+
+"""
+Podem ser concatenados usando o operador +
+e replicados usando o operador *
+também são aplicáveis para strings  += e *=
+
+A capacidade de usar o mesmo operador contra tipos de dados completamente
+diferentes (como números versus strings) é chamada de overloading 
+"""
+
+""" 
+Multiline
+/n invisivel  conta-  (\) não conta
+ """
+multiline = """Line #1
+Line #2"""
+
+print(len(multiline))
+
+### conhecer o valor do ponto de código ASCII/UNICODE de um caractere específico
+print(ord("a"))
+
+### obter o caractere correspondente
+print(chr(97))
+"""
+chr(ord(x)) == x
+ord(chr(x)) == x
+"""
+
+### index
+print("exemplo"[-1])
+
+### iterar
+the_string = 'silly walks'
+
+for character in the_string:
+    print(character, end=' ')
+
+### Slices
+alpha = "abdefg"
+
+print(alpha[1:3])
+print(alpha[3:])
+print(alpha[:3])
+print(alpha[3:-2])
+print(alpha[-3:4])
+print(alpha[::2])
+print(alpha[1::2])
+
+### in e not
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+print("f" in alphabet)
+print("F" in alphabet)
+print("1" in alphabet)
+print("ghi" in alphabet)
+print("Xyz" in alphabet)
+
+"""
+NÃO aceita del, append e insert
+Python strings são imutáveis
+
+del alphabet[0]  #error
+alphabet.append("A") #error
+alphabet.insert(0, "A") #error
+"""
+
+""" A única coisa que você pode fazer com del
+em uma string, é remover a string como um todo"""
+
+
+
+"""Isso pode """
+alphabet = alphabet + "z"
+alphabet = "a" + alphabet
+
+#min() -list or String
+print(min("aAbByYzZ"))
+
+t = 'The Knights Who Say "Ni!"'
+print('[' + min(t) + ']')
+
+"""Tabela ASCII – quais letras ocupam os
+ primeiros locais – superior ou inferior?"""
+
+#max() -list or String
+print(max("aAbByYzZ"))
+
+
+### index() method - retorna primeiro elemento
+print("aAbByYzZaA".index("b"))
+
+### list() function
+print(list("abcabc"))
+
+### count() method
+print("abcabc".count("b"))
+
+"""
+https://docs.python.org/3.4/library/stdtypes.html#string-methods
+"""
