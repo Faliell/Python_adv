f887a2746988bed54575098eed9435703aeaa5ad
Fabio Schapowal <fabiosw@me.com>
2023-10-15 08:34:20 +0100

    notes

--- Resume/notas.py
+++ Resume/notas.py
@@ -5,9 +5,7 @@ Python will created a reference to the same object.
 Therefore those two strings will have the same identity.
 """
 
-"""
-randint() the stop is inclusive.
-"""
+
 
 """
 isfile() 
@@ -16,8 +14,8 @@ eg. print(os.path.isfile('data.txt'))
 """
 
 # python aceita isso:
-if 10 > 1 < 10:
-    print(1)
+# if 10 > 1 < 10:
+#     print(1)
 
 
 """
@@ -99,4 +97,159 @@ randrange(1, 5)
 o segundo argumento do método  na biblioteca padrão Python não é inclusivo
 """
 
+"""
+randint(1, 5) the stop is inclusive.
+"""
 
+
+"""
+The read() function reads the whole file
+and returns a string.
+
+The readlines() function also reads the whole file
+and returns a list of the single lines.
+And you can also iterate through the file object.
+In each iteration you will get one line.
+"""
+
+
+"""
+%-20s é usado para formatar uma string com um espaço total de 20 caracteres,
+alinhando o texto à esquerda e preenchendo com espaços em branco à direita,
+se necessário.
+"""
+# texto = "Exemplo"
+# formatado = "%-20s bbb" % texto
+# print(formatado)  # Exemplo              bbb
+
+"""
+4:  largura total do campo em que o valor será inserido.
+Isso significa que o número será formatado para ocupar no mínimo 4 caracteres.
+.1: O ponto e o número seguinte (neste caso, 1) indicam quantas casas decimais
+devem ser exibidas após o ponto decimal no valor de ponto flutuante.
+f: Indica que o valor a ser inserido é um número de ponto flutuante.
+"""
+# numero = 12.345
+# formatado = "%4.1f" % numero
+# print(formatado)
+
+"""
+será tratado como um número inteiro na string formatada
+"""
+# numero = 42
+# formatado = "O número é: %d" % numero
+# print(formatado)
+
+
+"""
+valores a serem inseridos na string são passados como uma tupla
+(inteiro, ponto_flutuante) após o operador %.
+"""
+
+# inteiro = 42
+# ponto_flutuante = 12.345
+# frase = "Um inteiro: %d, ponto flutuante: %4.1f" % (inteiro, ponto_flutuante)
+# print(frase)
+
+"""
+Modo	Descrição
+'r'	Leitura (modo padrão). Abre o arquivo para leitura. Gera um erro
+se o arquivo não existir.
+'w'	Escrita. Abre o arquivo para escrita, truncando o arquivo (ou criando
+um novo se não existir).
+'a'	Anexação. Abre o arquivo para escrita, mas não trunca o arquivo,
+mantendo o conteúdo existente. Se o arquivo não existir, ele é criado.
+'x'	Criação exclusiva. Abre o arquivo para escrita, mas gera um erro se
+o arquivo já existir.
+'b'	Modo binário. Adicione 'b' ao modo para lidar com o arquivo em modo
+binário (por exemplo, 'rb' ou 'wb').
+'t'	Modo texto (padrão). Adicione 't' ao modo para lidar com o arquivo em
+modo de texto (por exemplo, 'rt' ou 'wt').
+'+'	Leitura e escrita. Adicione '+' ao modo (por exemplo, 'r+' ou 'w+')
+para permitir leitura e escrita no arquivo.
+"""
+
+
+"""
+errno.EEXIST - File exists
+errno.EBADF - Bad file number
+errno.EACCES - Permission denied
+"""
+
+
+"""
+terminal: python index.py Hello
+from sys import argv
+print(argv[1])
+First the name of the variable is argv (argument vector).
+And second the index 0 always holds the filename.
+Index 1 holds the first argument from the command line.
+"""
+
+
+# lista = [4]
+# print(lista[-1])  # 4
+
+#######################################
+# class A:
+#     def a(self):
+#         print('a')
+#
+# # class B(A)  dá erro de mro
+# class B:
+#     def a(self):
+#         print('b')
+#
+# # class C(B, A):  # Like this the result would be: b
+# class C(A, B):
+#     def c(self):
+#         self.a()
+#
+# c = C()
+# c.c()  # a
+# print(C.mro())
+#################################
+
+
+"""
+Em Python, os utilizadores podem definir excepções personalizadas criando
+uma nova classe.
+Esta classe de exceção tem de ser derivada,
+direta ou indiretamente, da classe Exception incorporada.
+"""
+
+"""
+The scientific notation always returns a float
+"""
+# amount = +42E7
+# print(type(amount))
+
+
+"""
+Parênteses ()
+Operadores de potenciação **
+Operadores unários + (positivo) e - (negativo)
+Operadores de multiplicação *, divisão /, e divisão inteira //
+Operador de módulo %
+Operadores de adição + e subtração -
+Operadores de comparação (por exemplo, <, <=, >, >=, ==, !=)
+Operadores lógicos and, or, not
+Operador de atribuição =
+"""
+
+
+"""
+fabs() returns the float value of the absolute value.
+floor() rounds a number down to the nearest integer
+Those are the two you need here.
+The others are of no use here:
+fmod() returns the float value of the remainder.
+ceil() rounds a number up to the nearest integer
+frexp() returns the mantissa and the exponent.
+"""
+# import math
+# print(math.floor(4.9))  # 4
+# print(math.fabs(-4))    # 4.0
+# print(math.frexp(4))    # (0.5, 3)
+# print(math.fmod(7, 2))  # 1.0
+# print(math.ceil(3.1))   # 4
